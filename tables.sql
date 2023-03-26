a0f877a38e0b9d0c2e20d4a25a22e6ad4ba2ba97
MoniqueSouza <moniquemoreira.mkt@gmail.com>
2023-03-26 12:14:35 -0300

    fix: Banco de dados

--- tables.sql
+++ tables.sql
@@ -1,3 +1,30 @@
+CREATE TABLE cakes (
+	"id" serial NOT NULL PRIMARY KEY,
+	"name" varchar(50) NOT NULL UNIQUE,
+	"price" numeric NOT NULL,
+	"image" varchar(255) NOT NULL,
+	"description" TEXT
+);
+
+CREATE TABLE clients (
+	"id" serial NOT NULL PRIMARY KEY,
+	"name" varchar(50) NOT NULL UNIQUE,
+	"adress" varchar(255) NOT NULL,
+	"phone" varchar(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE orders (
+	"id" serial NOT NULL,
+	"clientId" integer NOT NULL REFERENCES "clients"("id"),
+	"cakeId" integer NOT NULL REFERENCES "cakes"("id"),
+	"quantity" integer NOT NULL,
+	"totalprice" numeric NOT NULL,
+	"createdAt" timestamp without time zone DEFAULT now() NOT NULL
+);
+
+_____________________________________________________________________________
+
+
 CREATE TABLE "public.cakes" (
 	"id" serial NOT NULL,
 	"name" varchar(50) NOT NULL UNIQUE,
@@ -14,7 +41,7 @@ CREATE TABLE "public.cakes" (
 CREATE TABLE "public.clients" (
 	"id" serial NOT NULL,
 	"name" varchar(50) NOT NULL UNIQUE,
-	"adress" varchar(255) NOT NULL,
+	"address" varchar(255) NOT NULL,
 	"phone" varchar(50) NOT NULL UNIQUE,
 	CONSTRAINT "clients_pk" PRIMARY KEY ("id")
 ) WITH (
@@ -41,6 +68,7 @@ CREATE TABLE "public.orders" (
 
 ALTER TABLE "orders" ADD CONSTRAINT "orders_fk0" FOREIGN KEY ("clientId") REFERENCES "clients"("id");
 ALTER TABLE "orders" ADD CONSTRAINT "orders_fk1" FOREIGN KEY ("cakeId") REFERENCES "cakes"("id");
+
 
 
 
