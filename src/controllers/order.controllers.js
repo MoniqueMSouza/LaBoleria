aa909868498c83367e7513bedd67ce84acba9314
MoniqueSouza <moniquemoreira.mkt@gmail.com>
2023-03-27 09:33:50 -0300

    feat: GET /order

--- src/controllers/order.controllers.js
+++ src/controllers/order.controllers.js
@@ -1,19 +1,74 @@
 import { db } from '../database/database.js'
 
-export async function createOrder(req, res){
+export async function createOrder(req, res) {
 
-const {clientId, cakeId, quantity, totalPrice} = res.locals.orders
+    const { clientId, cakeId, quantity, totalPrice } = res.locals.orders
 
-try{
+    try {
 
-    await db.query(`
+        await db.query(`
     INSERT INTO orders ("clientId", "cakeId", quantity, "totalPrice") 
     VALUES ($1, $2, $3, $4);
     `, [clientId, cakeId, quantity, totalPrice])
 
 
-res.sendStatus(201)
-}catch(err){
-    res.status(500).send(err.message)
+        res.sendStatus(201)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
 }
+
+export async function getOrders(req, res) {
+    let date = ""
+    let infos = ""
+
+    if (req.query.date) {
+        date = req.query.date
+    }
+
+    try {
+        if (!date) {
+            infos = await db.query(`SELECT orders.*, cakes.*, clients.*,
+            orders.id AS id_order,cakes.id AS id_cake, clients.id AS id_clients, cakes.name AS cake_name, clients.name AS client_name 
+            FROM orders
+            JOIN cakes ON orders."cakeId" = cakes.id
+            JOIN clients ON orders."clientId" = clients.id`)
+        } else {
+            infos = await db.query(`SELECT orders.*, cakes.*, clients.*,
+            orders.id AS id_order,cakes.id AS id_cake, clients.id AS id_clients, cakes.name AS cake_name, clients.name AS client_name 
+            FROM orders
+            JOIN cakes on orders."cakeId" = cakes.id
+            JOIN clients on orders."clientId" = clients.id where "createdAt"::date = $1`, [date])
+        }
+
+const resposta = infos.rows.map((row) => ({
+    client: {
+      id: row.clientId,
+      name: row.client_name,
+      address: row.address,
+      phone: row.phone,
+    },
+    cake: {
+      id: row.cakeId,
+      name: row.cake_name,
+      price: row.price,
+      description: row.description,
+      image: row.image,
+    },
+    orderId: row.id_order,
+    createdAt: row.createdAt,
+    quantity: row.quantity,
+    totalPrice: row.totalPrice,
+  }));
+
+
+       
+
+
+        res.status(200).send(resposta)
+    } catch (err) {
+        res.status(500).send(err.message)
+
+    }
+
 }
--- src/routes/order.router.js
+++ src/routes/order.router.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { createOrder } from '../controllers/order.controllers.js';
 import {validSchemaOrders} from '../middlewares/orders.middlewares.js'
+import { getOrders } from '../controllers/order.controllers.js'
 
 
 const router = Router();
 
 router.post('/order',validSchemaOrders, createOrder);
-router.get('/order', () =>{});
+router.get('/order',  getOrders);
 router.get('/order/:id', ()=>{});
 
 export default router;
