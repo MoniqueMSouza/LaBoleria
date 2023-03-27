3ac0608ca295af53ee9885438c399041ffa89fd0
MoniqueSouza <moniquemoreira.mkt@gmail.com>
2023-03-27 09:08:12 -0300

    feat: GET /clients/:id/orders

--- src/controllers/clientes.controllers.js
+++ src/controllers/clientes.controllers.js
@@ -1,19 +1,47 @@
 import { db } from '../database/database.js'
 
-export async function createClients (req, res){
+export async function createClients(req, res) {
 
-const {name, address, phone} = res.locals.clients
+    const { name, address, phone } = res.locals.clients
 
-try{
+    try {
 
-    await db.query(`
+        await db.query(`
     INSERT INTO clients (name, address, phone) 
     VALUES ($1, $2, $3);
     `, [name, address, phone])
 
 
-res.sendStatus(201)
-}catch(err){
-    res.status(500).send(err.message)
+        res.sendStatus(201)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
 }
+
+
+export async function getClients(req, res) {
+    const { id } = req.params
+
+
+    try {
+        const infos = await db.query(`SELECT orders.*, cakes.*, orders.id as id_order,cakes.id as id_cake,cakes.name as cake_name FROM orders
+        join cakes on orders."cakeId" = cakes.id
+        WHERE orders."clientId" = $1`,[id])
+    
+        if (infos.rowCount === 0) return res.sendStatus(404)
+
+        const order = infos.rows.map((row) => ({
+            orderId: row.id_order,
+            quantity: row.quantity,
+            createdAt: row.createdAt,
+            totalPrice: row.totalPrice,
+            cakeName: row.cake_name
+          }));
+
+        
+        res.status(200).send(order);
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+
 }
--- src/routes/clientes.router.js
+++ src/routes/clientes.router.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { validSchemaClients } from '../middlewares/clientes.middlewares.js';
 import { createClients } from '../controllers/clientes.controllers.js';
+import {getClients} from '../controllers/clientes.controllers.js'
 
 
 const router = Router();
 
 router.post('/clients',validSchemaClients ,createClients);
-router.get('/clientes/:id/orders', ()=>{});
+router.get('/clientes/:id/orders', getClients);
 
 export default router;
